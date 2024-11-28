ec45d9cc2d565548644425defc37d8d801ad5421
malithrashmika <malithrashmika10@gmail.com>
2024-11-28 21:37:23 +0530

    login controller  done

--- src/main/java/lk/ijse/culinaryacademy/controller/LoginFormController.java
+++ src/main/java/lk/ijse/culinaryacademy/controller/LoginFormController.java
@@ -2,10 +2,23 @@ package lk.ijse.culinaryacademy.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.culinaryacademy.bo.BOFactory;
+import lk.ijse.culinaryacademy.bo.custom.AuthenticationBO;
+import lk.ijse.culinaryacademy.dto.UserDTO;
+import lk.ijse.culinaryacademy.exception.ExceptionHandler;
+import lk.ijse.culinaryacademy.exception.InvalidCredentialsException;
+import lk.ijse.culinaryacademy.util.PasswordStorage;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -21,24 +34,59 @@ public class LoginFormController {
     @FXML
     private AnchorPane loginForm;
 
+    AuthenticationBO authenticationBO= (AuthenticationBO) BOFactory.getBO(BOFactory.BOType.AUTH);
+
+    public static UserDTO userDTO;
+
     @FXML
     void goToSignUpOnAction(MouseEvent event) {
-
+        try {
+            loginForm.getChildren().setAll((Node) FXMLLoader.load(this.getClass().getResource("/signUpForm.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void inputPasswordOnAction(ActionEvent event) {
-
+        loginOnAction(event);
     }
 
     @FXML
     void inputUserNameOnAction(ActionEvent event) {
-
+        inputPassword.requestFocus();
     }
 
     @FXML
     void loginOnAction(ActionEvent event) {
+        if (!inputUserName.getText().isEmpty() && !inputPassword.getText().isEmpty()) {
+            try {
+                UserDTO loginUser = AuthenticationBO.getUser(inputUserName.getText().trim());
+                if (PasswordStorage.checkPassword(inputPassword.getText().trim(), loginUser.getPassword())){
+                    openMainForm();
+                    userDTO = loginUser;
+                } else {
+                    new Alert(Alert.AlertType.ERROR,"Invalid User Password !!").show();
+                }
+            } catch (InvalidCredentialsException e){
+                ExceptionHandler.handleException(e);
+            }
+        } else {
+            new Alert(Alert.AlertType.WARNING,"Please Enter All Fields !!").show();
+        }
+    }
 
+    private void openMainForm() {
+        try {
+            Scene scene = new Scene(FXMLLoader.load(this.getClass().getResource("/mainForm.fxml")));
+            Stage stage = (Stage) fullLoginForm.getScene().getWindow();
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/lk/ijse/culinaryacademy/controller/ProgramFormController.java
+++ src/main/java/lk/ijse/culinaryacademy/controller/ProgramFormController.java
@@ -1,12 +1,25 @@
 package lk.ijse.culinaryacademy.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import lk.ijse.culinaryacademy.bo.BOFactory;
+import lk.ijse.culinaryacademy.bo.custom.AcademicBO;
+import lk.ijse.culinaryacademy.dto.ProgramsDTO;
+import lk.ijse.culinaryacademy.exception.InUseException;
+import lk.ijse.culinaryacademy.exception.UserAlreadyExistsException;
+import lk.ijse.culinaryacademy.tdm.ProgramTm;
+import lk.ijse.culinaryacademy.util.Regex;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgramFormController {
 
@@ -23,7 +36,7 @@ public class ProgramFormController {
     private TableColumn<?, ?> colProgramName;
 
     @FXML
-    private TableView<?> tblProgram;
+    private TableView<ProgramTm> tblProgram;
 
     @FXML
     private TextField txtDuration;
@@ -37,64 +50,180 @@ public class ProgramFormController {
     @FXML
     private TextField txtName;
 
+    AcademicBO academicBO= (AcademicBO) BOFactory.getBO(BOFactory.BOType.ACADEMIC);
+
+    public void initialize() {
+        setCellValueFactory();
+        loadAllPrograms();
+    }
+
+    private void setCellValueFactory() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colProgramName.setCellValueFactory(new PropertyValueFactory<>("programName"));
+        colDuration.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        colFee.setCellValueFactory(new PropertyValueFactory<>("fee"));
+    }
+
+    private void loadAllPrograms() {
+        List<ProgramsDTO> programs =academicBO.getAllProgram();
+        tblProgram.getItems().clear();
+        ObservableList<ProgramTm> programTms = tblProgram.getItems();
+        for (ProgramsDTO program : programs) {
+            String duration = convertDurationToString(program.getDuration());
+            programTms.add(new ProgramTm(program.getProgramId(),program.getProgramName(),duration,program.getFee()));
+        }
+        tblProgram.setItems(programTms);
+    }
+
+    private String convertDurationToString(int duration) {
+        String durationString = "";
+
+        if(duration > 11){
+            int years = duration / 12;
+            int months = duration % 12;
+
+            if (months == 0){
+                durationString = years + " years";
+            } else {
+                durationString = years + " years " + months + " months";
+            }
+        } else {
+            durationString = duration + " months";
+        }
+        return durationString;
+    }
+
     @FXML
     void btnClearOnAction(ActionEvent event) {
+        clearData();
+    }
 
+    private void clearData() {
+        txtDuration.clear();
+        txtFee.clear();
+        txtId.clear();
+        txtName.clear();
+    }
+
+    private ProgramsDTO getObject(){
+        return new ProgramsDTO(txtId.getText(),txtName.getText(),Integer.parseInt(txtDuration.getText()),Double.parseDouble(txtFee.getText()),new ArrayList<>());
+    }
+
+
+    private int convertDurationToInt(String duration){
+        int years = 0;
+        int months = 0;
+
+        // Split the input string by spaces to identify the number and unit
+        String[] parts = duration.split(" ");
+
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i].equalsIgnoreCase("year") || parts[i].equalsIgnoreCase("years")) {
+                years = Integer.parseInt(parts[i - 1]); // Get the value before "year" or "years"
+            } else if (parts[i].equalsIgnoreCase("month") || parts[i].equalsIgnoreCase("months")) {
+                months = Integer.parseInt(parts[i - 1]); // Get the value before "month" or "months"
+            }
+        }
+
+        // Convert years to months and add to months
+        return (years * 12) + months;
+    }
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) throws InUseException {
+        if (!LoginFormController.userDTO.getRole().equals("Admissions Coordinator")){
+            if (isValied() && !txtId.getText().isEmpty()){
+                academicBO.deleteProgram(academicBO.getProgram(txtId.getText().trim()));
+                loadAllPrograms();
+                clearData();
+            } else {
+                new Alert(Alert.AlertType.WARNING,"Please Enter All Fields !!").show();
+            }
+        } else {
+            new Alert(Alert.AlertType.WARNING,"You cannot access this !!").show();
+        }
     }
 
     @FXML
-    void btnDeleteOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnSaveOnAction(ActionEvent event) {
-
+    void btnSaveOnAction(ActionEvent event) throws UserAlreadyExistsException {
+        if (!LoginFormController.userDTO.getRole().equals("Admissions Coordinator")){
+            if (isValied() && !txtId.getText().isEmpty()){
+                academicBO.saveProgram(academicBO.getProgram(txtId.getText().trim()));
+                loadAllPrograms();
+                clearData();
+            } else {
+                new Alert(Alert.AlertType.WARNING,"Please Enter All Fields !!").show();
+            }
+        } else {
+            new Alert(Alert.AlertType.WARNING,"You cannot access this !!").show();
+        }
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
-
+        if (!LoginFormController.userDTO.getRole().equals("Admissions Coordinator")){
+            if (isValied() && !txtId.getText().isEmpty()){
+                academicBO.updateProgram(academicBO.getProgram(txtId.getText().trim()));
+                loadAllPrograms();
+                clearData();
+            } else {
+                new Alert(Alert.AlertType.WARNING,"Please Enter All Fields !!").show();
+            }
+        } else {
+            new Alert(Alert.AlertType.WARNING,"You cannot access this !!").show();
+        }
     }
 
     @FXML
     void tblProgramOnClickAction(MouseEvent event) {
-
+        ProgramTm programTm = tblProgram.getSelectionModel().getSelectedItem();
+        if (programTm != null) {
+            txtId.setText(programTm.getId());
+            txtName.setText(programTm.getProgramName());
+            txtDuration.setText(String.valueOf(convertDurationToInt(programTm.getDuration().trim())));
+            txtFee.setText(String.valueOf(programTm.getFee()));
+        }
     }
 
     @FXML
     void txtDurationKeyAction(KeyEvent event) {
-
+        Regex.setTextColor(lk.ijse.culinaryacademy.util.TextField.MONTH, txtDuration);
     }
 
     @FXML
     void txtDurationOnAction(ActionEvent event) {
-
+        txtFee.requestFocus();
     }
 
     @FXML
     void txtFeeKeyAction(KeyEvent event) {
-
+        Regex.setTextColor(lk.ijse.culinaryacademy.util.TextField.PRICE, txtFee);
     }
 
     @FXML
     void txtIdKeyAction(KeyEvent event) {
-
+        Regex.setTextColor(lk.ijse.culinaryacademy.util.TextField.STUDENTID, txtId);
     }
 
     @FXML
     void txtIdOnAction(ActionEvent event) {
-
+        txtName.requestFocus();
     }
 
     @FXML
     void txtNameKeyAction(KeyEvent event) {
-
+        Regex.setTextColor(lk.ijse.culinaryacademy.util.TextField.NAME, txtName);
     }
 
     @FXML
     void txtNameOnAction(ActionEvent event) {
 
+    }
+    private boolean isValied() {
+        if (!Regex.setTextColor(lk.ijse.culinaryacademy.util.TextField.STUDENTID, txtId)) return false;
+        if (!Regex.setTextColor(lk.ijse.util.TextField.NAME, txtName)) return false;
+        if (!Regex.setTextColor(lk.ijse.util.TextField.MONTH, txtDuration)) return false;
+        if (!Regex.setTextColor(lk.ijse.util.TextField.PRICE, txtFee)) return false;
+        return true;
     }
 
 }
